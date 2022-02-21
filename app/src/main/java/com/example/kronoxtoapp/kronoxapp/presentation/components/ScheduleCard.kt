aed302428f6e23006f98c60cc004afab8eab47e3
adisve <adis.veletanlic@gmail.com>
2022-02-21 13:42:31 +0100

    Updated formatting of output

--- app/src/main/java/com/example/kronoxtoapp/kronoxapp/presentation/components/ScheduleCard.kt
+++ app/src/main/java/com/example/kronoxtoapp/kronoxapp/presentation/components/ScheduleCard.kt
@@ -1,5 +1,7 @@
 package com.example.kronoxtoapp.kronoxapp.presentation.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
@@ -11,7 +13,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.kronoxtoapp.kronoxapp.domain.model.ScheduleDetails
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun ScheduleCard(
     schedule: ScheduleDetails,
@@ -19,7 +26,7 @@ fun ScheduleCard(
 ){
     Card(
         shape = MaterialTheme.shapes.small,
-        contentColor = Color.Blue,
+        contentColor = Color.White,
         modifier = Modifier
             .padding(
                 bottom = 3.dp,
@@ -27,7 +34,8 @@ fun ScheduleCard(
             )
             .fillMaxWidth()
             .clickable(onClick = onClick),
-        elevation = 8.dp
+        elevation = 8.dp,
+        backgroundColor = Color(android.graphics.Color.parseColor("#" + "9a6eba"))
     ) {
         Column {
             schedule.title?.let { title ->
@@ -37,21 +45,32 @@ fun ScheduleCard(
                         .padding(top = 13.dp, bottom = 13.dp, start = 8.dp, end = 8.dp)
                 ){
                     Text(
-                        text = title,
+                        text = title + "\n" + schedule.location.toString(),
                         modifier = Modifier
-                            .fillMaxWidth(0.65f)
-                            .wrapContentWidth(Alignment.Start),
-                        style = MaterialTheme.typography.h5
-                    )
-                    Text(
-                        text = schedule.course.toString(),
-                        modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxWidth(0.45f)
                             .wrapContentWidth(Alignment.Start),
                         style = MaterialTheme.typography.subtitle1
+                    )
+                    Text(
+                        text = schedule.course.toString() +
+                                "\n"
+                                + LocalDateTime.parse(
+                                    schedule.start?.substring(
+                                        0, schedule.start?.length?.minus(6)!!)).format(DateTimeFormatter.ofLocalizedDateTime(
+                            FormatStyle.SHORT))
+                                + "\n" + LocalDateTime.parse(
+                            schedule.end?.substring(
+                                0, schedule.start?.length?.minus(6)!!)).format(DateTimeFormatter.ofLocalizedDateTime(
+                            FormatStyle.SHORT))
+                        ,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.End),
+                        style = MaterialTheme.typography.caption
                         )
                 }
             }
         }
     }
+
 }
--- app/src/main/java/com/example/kronoxtoapp/kronoxapp/presentation/ui/schedulelist/ScheduleListViewModel.kt
+++ app/src/main/java/com/example/kronoxtoapp/kronoxapp/presentation/ui/schedulelist/ScheduleListViewModel.kt
@@ -64,7 +64,6 @@ constructor(
                     }
                 }
             }
-
             schedules.value = scheduleList
         }
     }
